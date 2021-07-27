1fecba6f645e2512dd8dea772c46cc39387012c4
jahnvisugar <79485441+jahnvisugar@users.noreply.github.com>
2021-07-27 15:14:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var name_of_the_pizza_array=[];
+
+function submit(){
+    var name_1= document.getElementById("name_of_the_pizza_1").value;
+    var name_2= document.getElementById("name_of_the_pizza_2").value;
+    var name_3= document.getElementById("name_of_the_pizza_3").value;
+    var name_4= document.getElementById("name_of_the_pizza_4").value;
+
+    name_of_the_pizza_array.push(name_1);
+    name_of_the_pizza_array.push(name_2);
+    name_of_the_pizza_array.push(name_3);
+    name_of_the_pizza_array.push(name_4);
+    
+    console.log(name_of_the_pizza_array);
+    document.getElementById("display_name").innerHTML=name_of_the_pizza_array;
+    document.getElementById("submitbutton").style.display="inline-block";
+    document.getElementById("addbutton").style.display="none";
+
+    
+    
+}
+function add(){
+    name_of_the_pizza_array.add();
+    console.log(name_of_the_pizza_array);
+    document.getElementById("none").innerHTML=name_of_the_pizza_array;
+}
